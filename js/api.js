d26ea8622ec84d51c76be1026e5019d669068ae8
sndljpt42 <marwanali941@gmail.com>
2026-07-16 15:29:21 +0700

    non aktif preflight

--- js/api.js
+++ js/api.js
@@ -44,9 +44,9 @@ async function sendAttendance(token) {
     // Mengirim request POST
     const response = await fetch(API_URL, {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
+        // headers: {
+        //     "Content-Type": "application/json"
+        // },
         body: JSON.stringify(payload)
     });
 
