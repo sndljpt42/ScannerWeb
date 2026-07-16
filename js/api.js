911128175e4b0a9bafc58a69d3e4f285e80421bb
sndljpt42 <marwanali941@gmail.com>
2026-07-16 10:10:52 +0700

    update API_URL lagi

--- js/api.js
+++ js/api.js
@@ -20,7 +20,7 @@
 // Ganti dengan URL Web App Anda.
 // ============================================================
 const API_URL =
-    "https://script.google.com/macros/s/AKfycbyjTnEuAZ-A1RDqVWbm86TnG2GGD8SWG27MarJC68E41g1zHKN9MqEcERSoGwTEqdB_uA/exec";
+    "https://script.google.com/macros/s/AKfycbyXuHlgNcRQkkErhAm3JnPL0TAq1LcoChWO9VYBoaCvdguB-sPmLC-zj_lOrTdPRyUtsg/exec";
 
 /**
  * ============================================================
